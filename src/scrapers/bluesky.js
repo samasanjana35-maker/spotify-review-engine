75b394bc61d07834fa2a0f23b4e27b0f17b7e895
Sanjana Sama <sanjanasama@Sanjanas-MacBook-Air.local>
2026-06-24 20:07:53 +0530

    fix: broader Bluesky queries to get more posts for filtering

--- src/scrapers/bluesky.js
+++ src/scrapers/bluesky.js
@@ -3,7 +3,7 @@ const { isWithin90Days } = require('../utils/dateUtils');
 const logger = require('../utils/logger');
 
 const SEARCH_QUERIES = [
-  'spotify algorithm stuck', 'spotify same songs repeat', 'spotify recommend new music', 'spotify discover weekly broken',
+  'spotify music', 'spotify same songs repeat', 'spotify app', 'spotify discover weekly broken',
   'spotify recommendations algorithm',
   'spotify same songs repeat',
 ];
